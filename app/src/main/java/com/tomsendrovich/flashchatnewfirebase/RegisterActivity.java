981efc4e707884ba70b648d0d40c27308cb535e4
Tsanderovich <Tsanderovich@gmail.com>
2020-06-09 21:00:09 +0300

    Adding a SharedPreferences variable to store the username locally.

--- app/src/main/java/com/tomsendrovich/flashchatnewfirebase/RegisterActivity.java
+++ app/src/main/java/com/tomsendrovich/flashchatnewfirebase/RegisterActivity.java
@@ -1,5 +1,7 @@
 package com.tomsendrovich.flashchatnewfirebase;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
@@ -131,6 +133,11 @@ public class RegisterActivity extends AppCompatActivity {
                  if(!task.isSuccessful()){
                      Log.d("Flashchat","user creation failed");
                      showErrorDialog("Registration attempt failed");
+                 }else{
+                     saveDisplayName();
+                     Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                     finish();
+                     startActivity(intent);
                  }
 
             }
@@ -138,7 +145,12 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     // TODO: Save the display name to Shared Preferences
+    private void saveDisplayName(){
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS,0);
+        prefs.edit().putString(DISPLAY_NAME_KEY,displayName).apply();
 
+    }
 
     // TODO: Create an alert dialog to show in case registration failed
     private void showErrorDialog(String message){
